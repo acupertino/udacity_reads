83095dbb7d207057f97ba7e136b55fffc25b212b
Allan Cupertino <allan_csp@hotmail.com>
2018-09-05 17:17:26 -0300

    nada

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ class App extends React.Component {
       <div>
         <BookContainer filtrado1 = {filtro1}
                        filtrado2 = {filtro2}
-                       filtrado3 = {filtro3}/>
+                       filtrado3 = {filtro3}
+                       teste = {filtro1[0].url}/>
       </div>
   )
  }
--- src/BookContainer.js
+++ src/BookContainer.js
@@ -12,14 +12,14 @@ return(
         <div className="list-books-content">
               <div>
                 <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <h2 className="bookshelf-title">Currently Reading </h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       {this.props.filtrado1.map((a) => (
                         <li>
                             <div className="book">
                               <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(${a.url})" }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(${a.url})' }}></div>
                             
                               </div>
                               <div className="book-title">{a.name}</div>
