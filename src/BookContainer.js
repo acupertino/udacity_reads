ab9def7e1fe2b5784034c819a4ebe62b36d0c43b
Allan Cupertino <allan_csp@hotmail.com>
2018-09-11 01:52:14 -0300

    nada

--- src/BookContainer.js
+++ src/BookContainer.js
@@ -20,7 +20,15 @@ class BookContainer extends Component {
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
-
+                          <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading" selected = "true">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
                         </div>
                         <div className="book-title">{book.title}</div>
                         <div className="book-authors">{book.authors}</div>
@@ -44,10 +52,19 @@ class BookContainer extends Component {
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
-
+                          <div className="book-shelf-changer">
+                          <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading" >Currently Reading</option>
+                                <option value="wantToRead" selected = "true">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
                         </div>
                         <div className="book-title">{book.title}</div>
                         <div className="book-authors">{book.authors}</div>
+                        
                       </div>
                     </li>
                   ))}
@@ -65,10 +82,19 @@ class BookContainer extends Component {
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
-
+                          <div className="book-shelf-changer">
+                          <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading" >Currently Reading</option>
+                                <option value="wantToRead" >Want to Read</option>
+                                <option value="read" selected = "true">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
                         </div>
                         <div className="book-title">{book.title}</div>
                         <div className="book-authors">{book.authors}</div>
+                        
                       </div>
                     </li>
                   ))}
