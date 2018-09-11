7200435abc2a2a341f60c67318b0151af15bdc3a
Allan Cupertino <allan_csp@hotmail.com>
2018-09-11 01:34:07 -0300

    filtro ok

--- src/App.js
+++ src/App.js
@@ -6,33 +6,21 @@ import * as BooksAPI from './BooksAPI'
 
 class App extends React.Component {
   state = {
-    dados: [],
-    dados2: []
+    dados: []
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ dados: books })
     })
-
-    BooksAPI.get("nggnmAEACAAJ").then((teste) => {
-      this.setState({dados2 : teste})
-    })
   }
 
+  
 
   render() {
 
     return (
-      <div>
-
-        {this.state.dados.filter(book => book.shelf === 'wantToRead').map(book => (
-          <ol key={book.id}>{book.title}</ol>
-        )
-        )}
-
-        {this.state.dados2.title}
-      </div>
+      <BookContainer dados = {this.state.dados}/>
     )
   }
 }
--- src/BookContainer.js
+++ src/BookContainer.js
@@ -1,88 +1,89 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
 class BookContainer extends Component {
-render(){
-console.log('Props', this.props)
-return(
-    <div className="list-books">
+  render() {
+    console.log('Props', this.props)
+    return (
+      <div className="list-books">
         <div className="list-books-title"><h1>MyReads</h1></div>
 
         <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading </h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.props.filtrado1.map((a) => (
-                        <li>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(${a.url})' }}></div>
-                            
-                              </div>
-                              <div className="book-title">{a.name}</div>
-                              <div className="book-authors">{a.author}</div>
-                            </div>
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading </h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.dados.filter(book => book.shelf === 'currentlyReading').map(book => (
+                    <li>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
 
-
-
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.props.filtrado2.map((b) => (
-                        <li>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(${livros.quero.url})' }}></div>
-                                    
-                              </div>
-                              <div className="book-title">{b.name}</div>
-                              <div className="book-authors">{b.author}</div>
-                            </div>
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.props.filtrado3.map((c) => (
-                        <li>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(${livros_lido.url}' }}></div>
-                                   
-                              </div>
-                              <div className="book-title">{c.name}</div>
-                              <div className="book-authors">{c.author}</div>
-                            </div>
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                </div>
-
-                
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                        
+                      </div>
+                    </li>
+                  ))}
+                </ol>
               </div>
+            </div>
+
+
+
+
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.dados.filter(book => book.shelf === 'wantToRead').map(book => (
+                    <li>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
+
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+
+
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.dados.filter(book => book.shelf === 'read').map(book => (
+                    <li>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
+
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+
+
+          </div>
         </div>
-    </div>
+      </div>
     )
-}
+  }
 }
 
 export default BookContainer
-                    
-         
+
+
