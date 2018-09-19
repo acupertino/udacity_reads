4e466f2246a5f1d313d0f47ccce46a93f7c51c2f
Allan Cupertino <allan_csp@hotmail.com>
2018-09-19 02:36:54 -0300

    funfando real

--- src/AddBook.js
+++ src/AddBook.js
@@ -14,61 +14,66 @@ class AddBook extends React.Component {
     updateQuery = (query) => {
         str = query
         console.log(str)
-        BooksAPI.search(str).then((result) => {
-            if(result.error === "empty query"){
-                this.setState({ data: [] })
-            }else{
-                this.setState({ data: result })   
-            }
-  })}
+        if (str != "") {
+            BooksAPI.search(str).then((result) => {
+                if (result.error === "empty query") {
+                    this.setState({ data: [] })
+                } else {
+                    this.setState({ data: result })
+                }
 
-render() {
-    return (
-        <div>
-            {console.log('State', this.state.data)}
-            <div className="search-books-bar">
-                <div><Link to="/" className="close-search"></Link></div>
-                <input
-                    type="text"
-                    placeholder="Search by title or author"
-                    value={this.str}
-                    onChange={(event) => this.updateQuery(event.target.value)}>
-                </input>
-            </div>
-            <div className="search-books-results">
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {this.state.data.map(book => (
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }} />
-                                                    <div className="book-shelf-changer">
-                                                        <ButtonChange shelf={book.shelf}
-                                                            infoLivro={book}
-                                                            changeBook={this.props.changeBook} />
+            })
+        } else {
+            this.setState({ data: [] })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log('State', this.state.data)}
+                <div className="search-books-bar">
+                    <div><Link to="/" className="close-search"></Link></div>
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                        value={this.str}
+                        onChange={(event) => this.updateQuery(event.target.value)}>
+                    </input>
+                </div>
+                <div className="search-books-results">
+                    <div className="list-books-content">
+                        <div>
+                            <div className="bookshelf">
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {this.state.data.map(book => (
+                                            <li key={book.id}>
+                                                <div className="book">
+                                                    <div className="book-top">
+                                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }} />
+                                                        <div className="book-shelf-changer">
+                                                            <ButtonChange shelf={book.shelf}
+                                                                infoLivro={book}
+                                                                changeBook={this.props.changeBook} />
+                                                        </div>
                                                     </div>
+                                                    <div className="book-title">{book.title}</div>
+                                                    <div className="book-authors">{book.authors}</div>
                                                 </div>
-                                                <div className="book-title">{book.title}</div>
-                                                {console.log(book.shelf)}
-                                                <div className="book-authors">{book.authors}</div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ol>
+                                            </li>
+                                        ))}
+                                    </ol>
+                                </div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    )
-}
+        )
+    }
 
 }
 
