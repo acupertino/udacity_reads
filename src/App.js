c7b6220a193777eff6651bda5e4fe7a246fc255a
Allan Cupertino <allan_csp@hotmail.com>
2018-09-19 02:01:13 -0300

    as

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import AddBook from './AddBook';
 
-
 class App extends React.Component {
-  state = {
-    dados: []
+    state = { 
+      dados : []
   }
+
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
@@ -17,13 +17,11 @@ class App extends React.Component {
     })
   }
 
-
   changeBook = (book, newShelf) => {
-    var i=0, j;
+    var i=0;
     {for (i=0;i<this.state.dados.length;i++){
-      this.state.dados[i].id === book.id && ((this.state.dados[i].shelf = newShelf), (j = i))
+      this.state.dados[i].id === book.id && (this.state.dados[i].shelf = newShelf)
     }
-
     BooksAPI.update(book, newShelf).then(book => {
       this.setState(state => ({
         book : state.dados.concat([ book ])
@@ -37,9 +35,12 @@ class App extends React.Component {
       <div>
         <Route exact path="/" render={() => (
           <BookContainer dados={this.state.dados}
-            changeBook={this.changeBook} />
+                         changeBook={this.changeBook} />
         )} />
-        <Route path="/AddBooks" component={AddBook} />
+        <Route path="/AddBooks" render={() => (
+          <AddBook dados={this.state.dados}
+                   changeBook={this.changeBook} />
+        )} />
       </div>
 
     )
